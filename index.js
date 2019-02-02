50c9e19144c1426f8f8691eff88d0cff4ce79f33
Ali Sloyan <vsloyan@movista.ru>
2019-02-02 18:48:44 +0300

    Настроил сервер

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let mongoose = require("mongoose");
+const express = require("express");
+const app = express();
 const server = "0.0.0.0:27017"; // REPLACE WITH YOUR DB SERVER
 const database = "hookah"; // REPLACE WITH YOUR DB NAME
+const bodyParser = require("body-parser");
+
 class Database {
   constructor() {
     this._connect();
@@ -33,6 +37,26 @@ const InfoSchema = new Schema({
 
 var InfoModel = mongoose.model("info", InfoSchema, "info");
 
-InfoModel.findOne().then(response => {
-  console.log(response);
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/getTitle", (req, res) => {
+  InfoModel.findOne().then(response => {
+    res.send(response.title);
+  });
+});
+
+app.listen(8009, () => {
+  console.log("Ну здарова");
+  InfoModel.findOne().then(response => {
+    console.log(response.title);
+  });
 });
